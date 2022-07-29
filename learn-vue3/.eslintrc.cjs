bdf17c69a02384966aa176bdf9222ae764467e60
colorisland <chaewonstudy3@gmail.com>
2022-07-29 14:41:27 +0900

    vue

--- learn-vue3/.eslintrc.cjs
+++ learn-vue3/.eslintrc.cjs
@@ -2,10 +2,22 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
-}
+    "@vue/eslint-config-prettier",
+  ],
+  "prettier/prettier": [
+    "error",
+    {
+      singleQuote: true,
+      semi: true,
+      useTabs: true,
+      trailingComma: true,
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: "avoid",
+    },
+  ],
+};
