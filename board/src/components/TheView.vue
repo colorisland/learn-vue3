c9ebdc940df2ea639ade03a931e7650af8712969
colorisland <chaewonstudy3@gmail.com>
2022-08-04 17:32:57 +0900

    navvar

--- board/src/components/TheView.vue
+++ board/src/components/TheView.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="container py-4">hi</div>
+  <div class="container py-4">
+    <h3>편성표</h3>
+    <hr />
+    <div>
+      <vue-good-table :columns="columns" :rows="rows" />
+    </div>
+    <hr />
+    <button class="btn btn-info" @click="getInfo">Axios Click</button>
+    <div></div>
+  </div>
 </template>
 
-<script>
-export default {
-  components: {},
-  setup() {
-    return {};
-  },
+<script setup>
+import { ref } from "@vue/reactivity";
+import { VueGoodTable } from "vue-good-table-next";
+import axios from "axios";
+let rows = ref([]);
+
+let columns = ref([]);
+
+let getInfo = () => {
+  let url = "http://localhost:8080/customer/v1/info/108787351";
+  axios.get(url).then((response) => {
+    console.log(response);
+  });
 };
 </script>
 
--- board/src/main.js
+++ board/src/main.js
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "vue-good-table-next/dist/vue-good-table-next.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import "./assets/main.css";
-
-createApp(App).mount("#app");
 import "bootstrap/dist/js/bootstrap.js";
+import axios from "axios";
+
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+app.mount("#app");
