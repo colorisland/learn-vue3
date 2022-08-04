78f3715e40b6176a0d26b08575eb67143bae73ef
colorisland <chaewonstudy3@gmail.com>
2022-08-04 14:26:58 +0900

    board 초기화

---
+++ board/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <the-nav></the-nav>
+    <the-view></the-view>
+  </div>
+</template>
+<script setup>
+import TheNav from "./components/TheNav.vue";
+import TheView from "./components/TheView.vue";
+</script>
+
+<style scoped></style>
---
+++ board/src/components/TheView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container py-4">hi</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
